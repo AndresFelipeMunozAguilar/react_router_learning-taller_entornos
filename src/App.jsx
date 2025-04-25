0d8d53748a03f0e261d33bb7da828b618f3b0e17
AndresFelipeMunozAguilar <munozaguilarandresfelipe@gmail.com>
2025-04-25 13:41:18 -0500

    AGREGAR PAGINAS A LAS RUTAS Y MOSTRARLAS CON UN CLICK

--- src/App.jsx
+++ src/App.jsx
@@ -1,61 +1,80 @@
 import React from 'react';
-import { AppBar, Toolbar, Typography, Box, Link } from '@mui/material';
+import { AppBar, Toolbar, Typography, Box, Button } from '@mui/material';
 import './App.css';
-
+import { Outlet } from "react-router-dom";
+import { NavLink } from 'react-router';
+import Clients from './Pages/Clients.jsx';
+import Providers from './Pages/Providers.jsx';
+import Users from './Pages/Users.jsx';
+import LogOut from './Pages/LogOut.jsx';
 
 const App = () => {
   return (
-    <div>
-      {/* Barra superior */}
-      <AppBar position="static" sx={{ backgroundColor: '#096280' }}>
-        <Toolbar>
-          {/* Logo de React */}
-          <img
-            id="ReactMainLogo"
-            src="/src/assets/react.svg"
-            alt="React Logo"
-            style={{ marginRight: '16px', width: '40px', height: '40px' }}
-          />
-          {/* Título de la aplicación */}
-          <Typography variant="h1" component="div" sx={{ fontSize: '3rem' }}>
-            Admin Panel
-          </Typography>
-        </Toolbar>
-      </AppBar>
-
-      {/* Barra de navegación */}
-      <Box
-        component="nav"
-        sx={{
-          display: 'flex',
-          justifyContent: 'space-between',
-          alignItems: 'center',
-          backgroundColor: '#f5f5f5',
-          padding: '8px 16px',
-          marginTop: 0,
-          paddingX: '38px'
-        }}
-      >
-        <Box sx={{ display: 'flex', gap: '16px' }}>
-          <Link href="/clients" color="inherit" underline="hover">
-            Clientes
-          </Link>
-          <Link href="/provider" color="inherit" underline="hover">
-            Proveedor
-          </Link>
-          <Link href="/user" color="inherit" underline="hover">
-            Usuarios
-          </Link>
+    <>
+      <div>
+        {/* Barra superior */}
+        <AppBar position="static" sx={{ backgroundColor: '#096280' }}>
+          <Toolbar>
+            {/* Logo de React */}
+            <img
+              id="ReactMainLogo"
+              src="/src/assets/react.svg"
+              alt="React Logo"
+              style={{ marginRight: '16px', width: '40px', height: '40px' }}
+            />
+            {/* Título de la aplicación */}
+            <Typography variant="h1" component="div" sx={{ fontSize: '3rem' }}>
+              Admin Panel
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        {/* Barra de navegación */}
+        <Box
+          component="nav"
+          sx={{
+            display: 'flex',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+            backgroundColor: '#f5f5f5',
+            padding: '8px 16px',
+            marginTop: 0,
+            paddingX: '38px'
+          }}
+        >
+          <Box sx={{ display: 'flex', gap: '16px' }}>
+            <NavLink to="/clients"
+            >
+              <Button variant="text" size="small">
+                Clientes
+              </Button>
+            </NavLink>
+            <NavLink to="/providers" >
+              <Button variant="text" size="small">
+                Proveedores
+              </Button>
+            </NavLink>
+            <NavLink to="/users" >
+              <Button variant="text" size="small">
+                Usuarios
+              </Button>
+            </NavLink>
+          </Box>
+          <Box>
+            <NavLink to="/log-out">
+              <Button
+                variant="outlined"
+                size="small"
+                color="error"
+              >
+                Cerrar sesión
+              </Button>
+            </NavLink>
+          </Box>
         </Box>
+      </div>
 
-
-        <Box>
-          <Link href="/log-out" color="inherit" underline="hover">
-            Cerrar sesión
-          </Link>
-        </Box>
-      </Box>
-    </div>
+      <Outlet />
+    </>
   );
 };
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,24 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import { BrowserRouter, Routes, Route } from 'react-router'
+import Clients from './Pages/Clients.jsx'
+import Providers from './Pages/Providers.jsx'
+import Users from './Pages/Users.jsx'
+import Logout from './Pages/LogOut.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<Clients />} />
+          <Route path="clients" element={<Clients />} />
+          <Route path="providers" element={<Providers />} />
+          <Route path="users" element={<Users />} />
+          <Route path="log-out" element={<Logout />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
