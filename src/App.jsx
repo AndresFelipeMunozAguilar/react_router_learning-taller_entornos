bcdf78f53cbf11dcf94d3b06c413efbd2f4de3c6
AndresFelipeMunozAguilar <munozaguilarandresfelipe@gmail.com>
2025-04-25 14:50:46 -0500

    resaltar el botón de la página activa, a traves de la variante del botón

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,31 @@ import { AppBar, Toolbar, Typography, Box, Button } from '@mui/material';
 import './App.css';
 import { Outlet } from "react-router-dom";
 import { NavLink } from 'react-router';
-import Clients from './Pages/Clients.jsx';
-import Providers from './Pages/Providers.jsx';
-import Users from './Pages/Users.jsx';
-import LogOut from './Pages/LogOut.jsx';
+import { useState } from 'react';
+import { getAllJSDocTagsOfKind } from 'typescript';
+
 
 const App = () => {
+
+  const defaultButton = 1
+  const disabledBttnColor = 'inherit'
+
+  // Función para manejar el clic en los botones de navegación
+
+  /* La presente función funciona con números, de tal forma
+   * que el número seleccionado indica qué botón está activo.
+   * Por ejemplo, si el botón 1 está activo, el número 1 indica
+   * que está activo el botón clientes. La selección de botones sigue
+   * la siguiente lógica:
+   * 
+   *     1. Clientes
+   *     2. Proveedores
+   *     3. Usuarios
+   *     4. Cerrar sesión
+   */
+  const [currentButton, setCurrentButton] = useState(defaultButton);
+
+
   return (
     <>
       <div>
@@ -44,17 +63,37 @@ const App = () => {
           <Box sx={{ display: 'flex', gap: '16px' }}>
             <NavLink to="/clients"
             >
-              <Button variant="text" size="small">
+              <Button
+                variant={currentButton === 1 ? "contained" : "text"}
+                size="small"
+                onClick={() => {
+                  setCurrentButton(1);
+                }}
+
+
+              >
                 Clientes
               </Button>
             </NavLink>
             <NavLink to="/providers" >
-              <Button variant="text" size="small">
+              <Button
+                variant={currentButton === 2 ? "contained" : "text"}
+                size="small"
+                onClick={() => {
+                  setCurrentButton(2);
+                }}
+              >
                 Proveedores
               </Button>
             </NavLink>
             <NavLink to="/users" >
-              <Button variant="text" size="small">
+              <Button
+                variant={currentButton === 3 ? "contained" : "text"}
+                size="small"
+                onClick={() => {
+                  setCurrentButton(3);
+                }}
+              >
                 Usuarios
               </Button>
             </NavLink>
@@ -62,9 +101,12 @@ const App = () => {
           <Box>
             <NavLink to="/log-out">
               <Button
-                variant="outlined"
+                variant={currentButton === 4 ? "contained" : "outlined"}
                 size="small"
                 color="error"
+                onClick={() => {
+                  setCurrentButton(4);
+                }}
               >
                 Cerrar sesión
               </Button>
