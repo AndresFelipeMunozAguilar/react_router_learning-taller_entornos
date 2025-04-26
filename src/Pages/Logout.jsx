0bd68d4812526121f3a58b2fbd1c8a4f0f72a8d9
AndresFelipeMunozAguilar <munozaguilarandresfelipe@gmail.com>
2025-04-25 23:59:28 -0500

    crear página de log out para manejar el cierre de sesión

--- src/Pages/Logout.jsx
+++ src/Pages/Logout.jsx
@@ -1,11 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import CheckCircleIcon from '@mui/icons-material/CheckCircle';
 
 const Logout = () => {
+    const [loggedOut, setLoggedOut] = useState(false);
+    const navigate = useNavigate();
+
+    const handleNo = () => {
+        navigate('/clients');
+    };
+
+    const handleYes = () => {
+        setLoggedOut(true);
+    };
+
     return (
-        <div>
-            <h1>Cerrar sesión</h1>
+        <div style="{{ margin: '2rem'}}">
+            {!loggedOut ? (
+                <div>
+                    <h1>¿Quieres cerrar sesión?</h1>
+                    <button onClick={handleYes}>Sí</button>
+                    <button onClick={handleNo}>No</button>
+                </div>
+            ) : (
+                <div>
+                    <h1>Sesión cerrada exitosamente</h1>
+                    <CheckCircleIcon style={{ color: 'green', fontSize: '2rem' }} />
+                </div>
+            )}
         </div>
-    )
-}
+    );
+};
 
 export default Logout;
